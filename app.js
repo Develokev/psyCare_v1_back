42bf97ca39f0e780a3a6c74922d4317523eea911
Develokev <develokev@gmail.com>
2023-10-18 15:24:45 +0200

    app.use(routers) enabled

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const port = process.env.Port || 3000;
 // Parse - translating json
 app.use(express.json());
 // To parse req with urlenconded payload
-app.use(express.urlenconded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 //Routes - prefix to make requests from back to BBDD.
-app.use('/admin/users', require('/routers/userRouters'))
+//app.use('/admin/users', require('/routers/userRouters'))
 
 app.use((req, res, next) => {
     res.status(404).send("404", {
