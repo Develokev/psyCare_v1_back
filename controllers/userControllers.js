01917f436b60c02457b15001da482d6dfc7f8897
Develokev <develokev@gmail.com>
2023-11-13 19:05:09 +0100

    controller documentation done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,11 @@
+/**DOCS
+ * Controladores de las rutas que filtran la información y data a través de los scripts en el front.
+ * Se encargan de manejar la información y realizar validaciones antes de enviarlas a los modelos(models) para que lleguen correctamente.
+ * 
+ * Se llama a generateToken(helper) para el encryptado de contraseña y generación de token de usuario.
+ */
 const { generateToken } = require("../helpers/generateJWT");
+
 const {
   getAllPatientsMod,
   getPatientByEmailMod,
@@ -8,6 +15,15 @@ const {
   updatePatientMod,
 } = require("../models/userModel");
 
+/**DOCS
+ * Controlador que hace la consulta a la BBDD y devuelve todos los pacientes.
+ * @method getAllPatientsControl
+ * @async
+ * @param {Object} req requerimiento de la ruta.
+ * @param {Object} res respuesta de la ruta.
+ * @returns {json} devuelve "data" con la información de todos los pacientes en la BBDD json - data
+ * @throws {error} devuelve error si hay un problema en la petición a la BBDD.
+ */
 const getAllPatientsControl = async (req, res) => {
   let data;
 
@@ -28,6 +44,15 @@ const getAllPatientsControl = async (req, res) => {
   }
 };
 
+/**DOCS
+ * Controlador que hace la consulta a la BBDD y busca un paciente por correo(email).
+ * @method getPatientByEmailControl
+ * @async
+ * @param {Object} req requerimiento de la ruta. Debe obtener el "email" desde los params de la ruta.
+ * @param {Object} res respuesta de la ruta.
+ * @returns {json} devuelve "data" con la información de un paciente coincidente con dicho correo(email) en la BBDD json - data
+ * @throws {error} devuelve error si hay un problema en la petición a la BBDD o si no ha encontrado ningún usuario bajo ese correo(email).
+ */
 const getPatientByEmailControl = async (req, res) => {
   let data;
   const email = req.params.email;
@@ -48,6 +73,18 @@ const getPatientByEmailControl = async (req, res) => {
   }
 };
 
+/**DOCS
+ * Controlador que hace la consulta a la BBDD y crea un usuario(paciente) nuevo.
+ * @method createPatientControl
+ * @async
+ * @param {Object} req requerimiento de la ruta. Deberá obtener del "body" el correo(email) del usuario a crear.
+ * @param {Object} res respuesta de la ruta.
+ * @param {Object} dataRole recibe a través del "body" un json con la data para crear el nuevo usuario
+ * dataRole = rol(patient by default), avatar(opcional), nombre, apellido, correo y contraseña.
+ * se obtiene también de dataRole el nombre y el rol para utilizarlos en la generación del token.
+ * @returns {json} devuelve la data con el paciente(user) nuevo y el "token" generado con el helper - data, token.
+ * @throws {error} devuelve error si hay un problema en la petición a la BBDD o si los datos en el body no se han enviado correctamente.
+ */
 const createPatientControl = async (req, res) => {
   const email = req.body.email;
 
@@ -85,34 +122,19 @@ const createPatientControl = async (req, res) => {
   }
 };
 
-const deletePatientControl = async (req, res) => {
-  let exist, data;
-  const id = req.params.id;
-
-  try {
-    exist = await getPatientByIdMod(id);
-
-    if (exist.rowCount > 0) {
-      data = await deletePatientMod(id);
-      res.status(200).json({
-        ok: true,
-        data: data.rowCount,
-        msg: "if data = 1, user successfully created",
-      });
-    } else {
-      res.status(400).json({
-        ok: false,
-        msg: "deleting patient FAILED",
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "delete patient control FAILED, please contact ADMIN",
-    });
-  }
-};
-
+/**DOCS
+ * Controlador que hace la consulta a la BBDD y actualiza la información de un usuario(paciente) por user_id(id).
+ * @method updatePatientControl
+ * @async
+ * @param {Object} req requerimiento de la ruta. Deberá obtener del "body" la data nueva a actualizar en el usuario(paciente) y del params el "id".
+ * @param {Object} res respuesta de la ruta.
+ * @param {String} id recibe a través de "params" un string con el "id"(user_id) del usuario que a actualizar.
+ * @param {Object} body recibe a través del "body" un json con la data para actualizar el nuevo usuario
+ * body = nombre, apellido, password y avatar.
+ * se obtiene también de "body" el nombre y el rol para utilizarlos en la generación del token al actualizar.
+ * @returns {json} devuelve la data actualizada con el paciente(user) nuevo y el "token" generado con el helper - data, token.
+ * @throws {error} devuelve error si hay un problema en la petición a la BBDD o si los datos en el body no se han enviado correctamente.
+ */
 const updatePatientControl = async (req, res) => {
   let data, id;
   id = req.params.id;
@@ -145,6 +167,45 @@ const updatePatientControl = async (req, res) => {
     res.status(500).json({
       ok: false,
       msg: "update patient model FAILED",
+    });
+  }
+};
+
+/**DOCS
+ * Controlador que hace la consulta a la BBDD y elimina un usuario(paciente) por user_id(id).
+ * @method deletePatientControl
+ * @async
+ * @param {Object} req requerimiento de la ruta. Deberá obtener el user_id(id) de los params.
+ * @param {Object} res respuesta de la ruta.
+ * @param {String} id recibe a través de "params" un string con el "id"(user_id) del usuario que se va a eliminar.
+ * Primero verifica que el email exista, y luego ejecuta el modelo para eliminar.
+ * @returns {json} devuelve true si ha eliminado correctamente el usuario.
+ * @throws {error} devuelve error si hay un problema en la petición a la BBDD o no se ha encontrado el usuario con "id" coincidente.
+ */
+const deletePatientControl = async (req, res) => {
+  let exist, data;
+  const id = req.params.id;
+
+  try {
+    exist = await getPatientByIdMod(id);
+
+    if (exist.rowCount > 0) {
+      data = await deletePatientMod(id);
+      res.status(200).json({
+        ok: true,
+        data: data.rowCount,
+        msg: "if data = 1, user successfully created",
+      });
+    } else {
+      res.status(400).json({
+        ok: false,
+        msg: "deleting patient FAILED",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "delete patient control FAILED, please contact ADMIN",
     });
   }
 };
