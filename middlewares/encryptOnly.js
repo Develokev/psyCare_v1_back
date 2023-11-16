bc561e4729b482f5ee0a1165c692f73f38836994
Develokev <develokev@gmail.com>
2023-11-16 15:31:06 +0100

    helper nd middlewares well documented

--- middlewares/encryptOnly.js
+++ middlewares/encryptOnly.js
@@ -1,6 +1,24 @@
+/**DOCS
+ * Middleware que se encarga únicamente de encriptar la contraseña que recibe en la solicitud del usuario.
+ */
+
+/** @module auth */
 const bcrypt = require("bcrypt");
 
+/**DOCS
+ * @async
+ * @method middleware
+ * @param {Object} req - requerimiento de la ruta - solicitud a Express.
+ * @param {Object} res - respuesta de la ruta - solicitud a Express.
+ * @param {Function} next - middleware de express que hace que la función no se detenga independiente del respuesta en caso de que la haya.
+ * @returns {String} devuelve la contraseña encriptada.
+ * @throws {Error} devuelve un error en forma de Json si hay un problema en la petición.
+ */
 const encryptPass = async (req,res,next) => {
+
+    /**
+    * @constant {String} password - recibe la contraseña proporcionada por el usuario en la solicitud.
+    */
     const { password } = req.body;
 
     if(!req.body) {
--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,6 +1,23 @@
+/**DOCS
+ * Middleware que se encarga de validar un token JWT en la solicitud.
+ */
+
+/** @module auth */
 const jwt = require('jsonwebtoken');
 
+/**
+ * @async
+ * @param {Object} req - requerimiento de la ruta - solicitud a Express.
+ * @param {Object} res - respuesta de la ruta - solicitud a Express.
+ * @param {Function} next - middleware de express que hace que la función no se detenga independiente del respuesta en caso de que la haya.
+ * @returns {JSON} devuelve true si existe token y si la verificación del mismo es correcta.
+ * @throws {Error} devuelve error si hay un problema en al solicitud y un mensaje de "invalid token".
+ */
 const validateJWT =async (req,res,next) => {
+
+    /**
+    * @constant {String} token - Token JWT extraído de la cabecera 'x-token'.
+    */
     const token = req.header('x-token');
     console.log(token);
 
@@ -12,6 +29,9 @@ const validateJWT =async (req,res,next) => {
     }
     
     try {
+        /**
+        * @constant {Object} payload - Contenido decodificado del token JWT. Recibe el user_id y el name del usuario.
+        */
         const payload = jwt.verify(token,process.env.JWT_SECRET_KEY);
 
         req.uid = payload.uid;
