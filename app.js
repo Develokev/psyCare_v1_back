b7d90b30a26b6a8b3be56d2e168b1898fea4e091
Develokev <develokev@gmail.com>
2023-11-18 15:06:12 +0100

    port Fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors());
 
 require('dotenv').config();
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 // Parse - translating json
 app.use(express.json());
--- config/configElephantSQL.js
+++ config/configElephantSQL.js
@@ -1,4 +1,4 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
     host: process.env.ELEPHANT_HOST,
@@ -7,4 +7,4 @@ const pool = new Pool({
     password: process.env.ELEPHANT_PASS,
   });
 
-module.exports = {pool};
+module.exports = { pool };
